c8eed8584a8a5166d6f72982b4949ce9d49319ef
Eduardo Gouveia <eduardogouveia74@gmail.com>
2026-07-23 21:41:12 -0300

    Add files via upload

---
+++ Operadores/PrecedenciaOperadoresAtribuicao.java
@@ -0,0 +1,16 @@
+public class PrecedenciaOperadoresAtribuicao {
+    public static void main (String[] args) {
+		int numero = 5;
+
+		System.out.println("Pré-incremento(++numero): " + (++numero));
+
+		System.out.println("Pós-incremento(numero++): " + (numero++));
+		System.out.println("Valor após o pós-incremento: " + numero);
+
+		System.out.println("Pré-decremento(--numero): " + (--numero));
+
+		System.out.println("Pós-decremento(numero--): " + (numero--));
+		System.out.println("Valor após o pós-decremento: " + numero);
+	}	
+
+}    		
