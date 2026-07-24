eadb45318fc799e819da9b0cf5d5c6e7653b43de
Eduardo Gouveia <eduardogouveia74@gmail.com>
2026-07-23 21:47:14 -0300

    Add files via upload

---
+++ Fundamentos/PromocaoAutomatica.java
@@ -0,0 +1,13 @@
+public class PromocaoAutomatica {
+    public static void main (String[] args) {
+		byte a = 11;
+		short b = 21;
+        char c = 'E'; //O tipo char e usado para representar caracteres, como letras e símbolos
+        int d = 31;
+        long e = 40L;
+        float f = 1.5f;
+        double g = 2.5;
+
+       System.out.println("Resultado da soma= " + c);
+    }
+}    	
