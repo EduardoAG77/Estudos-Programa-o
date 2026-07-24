f63d8ac9ca52e1f88e44e76eee2fa36536e4bbe0
Eduardo Gouveia <eduardogouveia74@gmail.com>
2026-07-23 21:46:26 -0300

    Add files via upload

---
+++ Fundamentos/CalculadoraAreaRetangulo.java
@@ -0,0 +1,8 @@
+public class CalculadoraAreaRetangulo {
+    public static void main (String[] args) {
+		double largura = 3.5;
+		double altura = 2.0;
+
+       System.out.println("Area do Retangulo= " + largura*altura);
+    }
+}    		
